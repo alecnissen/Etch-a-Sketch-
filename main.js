b9a9e60f98a0ec204b8f8a8fd784c4a082c8c7b1
alecnissen <alecnissen@yahoo.com>
2022-08-20 19:13:24 -0400

    added button to the top of the  page which prompts a user to enter the number of squares per side for the new grid

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ const cdiv = document.querySelector('.container');
 
 for (let i = 1; i < 273; i++) { 
     const div = document.createElement('div'); 
-    div.addEventListener('mouseover', e => e.target.classList.add('my-color-class'))
-    cdiv.appendChild(div); 
+    div.addEventListener('mouseover', e => e.target.classList.add('my-color-class')); 
+    cdiv.appendChild(div);   
 }     
 
+
+function gridPrompt() { 
+    // button.addEventListener('click', prompt('Enter')); 
+    prompt('Enter the number of square per side for a new grid'); 
+}
+
+ 
 
 // let changeColorOfDivs = cdiv.addEventListener('mouseover', e =>  { 
 //     cdiv.style.backgroundColor = 'red'; 
