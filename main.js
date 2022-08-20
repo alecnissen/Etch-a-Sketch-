90eadf6a507fe1394b4413d9b87e55c611986a8a
alecnissen <alecnissen@yahoo.com>
2022-08-19 22:39:26 -0400

    created an event listener function, mouseover event, changes the color to blue

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const cdiv = document.querySelector('.container'); 
 
+
 // Add 16 divs 
+// find a way to add each grid item not the container  
 
 for (let i = 1; i < 273; i++) { 
     const div = document.createElement('div'); 
+    div.addEventListener('mouseover', e => e.target.classList.add('my-color-class'))
     cdiv.appendChild(div); 
 }     
 
+
+// let changeColorOfDivs = cdiv.addEventListener('mouseover', e =>  { 
+//     cdiv.style.backgroundColor = 'red'; 
+// }) 
+
+// let returnColorOfDivs = cdiv.addEventListener('mouseout', (e) => { 
+//     cdiv.style.backgroundColor = 'white';
+// })  
